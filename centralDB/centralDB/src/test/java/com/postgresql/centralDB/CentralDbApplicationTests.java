a4ac2bcbfe56d978af828cd06ab4c681f4ef9fc4
Lascelle Mckenzie <lascellemckenzie@gmail.com>
2026-07-27 14:15:23 -0500

    Java Files Added

---
+++ centralDB/centralDB/src/test/java/com/postgresql/centralDB/CentralDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.postgresql.centralDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
